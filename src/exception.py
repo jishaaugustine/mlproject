506cd51a34ccd5db6bd0877556f93ed30947d58f
jishaaugustine <jishjuan@gmail.com>
2025-01-20 14:25:40 +0530

    EDA and Training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
@@ -15,3 +16,4 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
