0885f14053264ae124eca76fb62ac296bd36f791
jishaaugustine <jishjuan@gmail.com>
2025-01-24 13:26:20 +0530

    web app is created

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 from src.exception import CustomException
 
@@ -45,4 +46,11 @@ def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e,sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
    
